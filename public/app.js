abed3a9ee47161ca523ba0f77c92eaa7ad3850c2
Fabian Soriano <fabo.soriano@gmail.com>
2022-08-29 18:52:18 -0700

    added some instruction notes for future reference

--- public/app.js
+++ public/app.js
@@ -1,26 +1,29 @@
 // get user's data
-// Get the user's coordinates:                                                              
+// Get the user's coordinates:
+// We know the advertisement shouldn't attempt to display a message until we have the user's coordinates. 
+// That makes this an asynchronous task. You can use a promise to make async/await syntax available and avoid callbacks.      
+// First, resolve the position data and then return the latitude and longitude of the object via coords.latitude and coords.longitude. 
+// Use async/await syntax to return the coordinates array after the promise is fulfilled.                                                        
 async function getCoords(){
     pos = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
     return [pos.coords.latitude, pos.coords.longitude]
 }
-
-console.log(getCoords());
+// console.log(getCoords());
 
 
 // get user's time
 function userTime(){
     const now = new Date()
-    return now.getHours()
+    return now.getHours()      // 
 }
-console.log(userTime())
+// console.log(userTime())
 
 // helper functions
 // check time of day
 function getMealTime(){
-    const tod = userTime()
+    const tod = userTime()  // tod stands for time of day.
     // if(tod > 20){return 'late night snack'}
     // else if (tod > 16) {return 'dinner'}
     // else if (tod > 11) {return 'lunch'}
@@ -28,7 +31,7 @@ function getMealTime(){
     return tod > 20 ? 'late night snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast'
 }
 
-// console.log(getMealTime())
+// console.log(getMealTime())   time of day console logs and type of meal on developer tools in web browser
 
 // build ads
 // build ad 1
